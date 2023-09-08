65ad72d48163a3d015707f47c4985f0e96be6d31
chessboy71 <bentegrimohamed@gmail.com>
2023-09-08 04:22:54 -0700

    task 3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n = a;
+
+	for (n; n <= "z"; n++)
+		putchar(n);
+
+	return (0);
+}
