9347d424faf72aaf37e5f6b3cfdf0f0edb72136f
chessboy71 <bentegrimohamed@gmail.com>
2023-09-08 15:54:24 -0700

    task 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n < 57; n++)
+	{
+		for (m = n + 1; m <= 57; m++)
+		{
+			putchar((char)n);
+			putchar((char)m);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
