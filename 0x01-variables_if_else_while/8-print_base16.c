1ba112984aef935f9884fd244a9ca6cf011fc3b1
chessboy71 <bentegrimohamed@gmail.com>
2023-09-08 07:04:05 -0700

    task 9x

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n <= 57; n++)
+		putchar((char)n);
+
+	for (n = 97; n <= 102; n++)
+                putchar((char)m);
+
+	putchar('\n');
+	return (0);
+}
