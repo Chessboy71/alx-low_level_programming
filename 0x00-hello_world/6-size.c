88819f4d5f2e5bb60c219294ce998eaff31e3a22
chessboy71 <bentegrimohamed@gmail.com>
2023-09-07 18:05:27 -0700

    6th task

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main() {
+  printf("Size of int: %zu\n", sizeof(int));
+  printf("Size of char: %zu\n", sizeof(char));
+  printf("Size of float: %zu\n", sizeof(float));
+  printf("Size of double: %zu\n", sizeof(double));
+  printf("Size of long int: %zu\n", sizeof(long int));
+  printf("Size of long long int: %zu\n", sizeof(long long int));
+
+  return 0;
+}
+
