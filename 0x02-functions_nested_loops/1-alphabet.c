2659a2a1d82a8158f9dd57d985f52775a03e4489
chessboy71 <bentegrimohamed@gmail.com>
2023-09-12 19:44:08 +0100

    s

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,12 +7,7 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{
-
-	char i;
-
-	for (i = 'a'; i <= 'z'; i++)
-		_putchar(i);
-	_putchar('\n');
+}
+	print_alphabet();
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+#include "main.h"
+
+/**
+ * A function that prints the alphabet
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char i;
+	
+	for(i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+}
+
