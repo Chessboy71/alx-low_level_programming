c5da11f3dfee1c8f5d22127abd46bf4094391d9b
chessboy71 <bentegrimohamed@gmail.com>
2023-09-13 02:07:18 +0100

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * jack_bauer - prints the alphabet ten times
+ *
+ * Return: Always 0 (Success)
+ */
+void jack_bauer(void)
+{
+	int hours = 0;
+	int mins = 0;
+
+	while (hours < 60)
+	{
+		while (mins < 60)
+		{
+			_putchar('0' + (int)hours / 10);
+			_putchar('0' + hours % 10);
+			_putchar(':');
+			_putchar('0' + (int)min / 10);
+			_putchar('0' + mins % 10);
+			mins++;
+		}
+		hours++;
+	}
+	return (0);
+}
