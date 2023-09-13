4808ce82fcd0f683415fd5132543aa6cb69f3324
chessboy71 <bentegrimohamed@gmail.com>
2023-09-13 01:43:51 +0100

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_last_digit - prints the alphabet ten times
+ * @c : integer
+ *
+ * Return: Always 0 (Success)
+ */
+int print_last_digit(int c)
+{
+	return (c % 10);
+}
