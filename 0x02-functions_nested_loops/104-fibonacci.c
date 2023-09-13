7b4a9a214064ba4c850fdb86ccd366ecfccf4638
chessboy71 <bentegrimohamed@gmail.com>
2023-09-13 04:34:07 -0700

    task 103

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,23 +10,21 @@
 
 int main(void)
 {
-	long int fib;
 	int i;
 	long int num;
 	long int numm;
 
 
 	num = 1;
-	printf("%ld\n", num);
+	printf("%ld, ", num);
 	numm = 2;
-	printf("%ld\n", numm);
+	printf("%ld, ", numm);
 
 	for (i = 3; i <= 98; i++)
 	{
-		fib = num + numm;
-		printf("%ld, ", fib);
-		numm = fib;
-		num = numm;
+		printf("%ld, ", num + numm);
+		numm = num + numm;
+		num = numm - num;
 	}
 	return (0);
 }
