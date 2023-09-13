0395b281147bbd588916384c62c2fcf7aec70c60
chessboy71 <bentegrimohamed@gmail.com>
2023-09-13 01:23:02 +0100

    task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 
 /**
- * _islower - prints the alphabet ten times
+ * _isalpha - prints the alphabet ten times
  * @c : integer
  *
  * Return: Always 0 (Success)
  */
-int _islower(int c)
+int _isalpha(int c)
 {
 	if (c > 65 && c < 91 || c > 96 && c < 123)
 		return (1);
