18ec3d9750154aac048306743e203e670d94adcd
chessboy71 <bentegrimohamed@gmail.com>
2023-09-13 02:47:21 +0100

    s

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,26 +8,29 @@
  */
 void times_table(void)
 {
-	int i;
-	int j;
+	int i j;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 9; j++)
 		{
-			if (j * i < 10)
+			if (i == 0)
 			{
+				_putchar('0');
+			}
+			else if (j * i < 10)
+			{
+			_putchar(' ');
+			_putchar(' ');
 			_putchar('0' + j * i);
 			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
 			}
 			else
 			{
+			_putchar(' ');
 			_putchar('0' + (int)(j * i / 10));
 			_putchar('0' + (j * i) % 10);
 			_putchar(',');
-			_putchar(' ');
 			}
 		}
 		if (j * i < 10)
