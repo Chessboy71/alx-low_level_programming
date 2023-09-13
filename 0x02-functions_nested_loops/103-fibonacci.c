f96130314e533653c5b722b3f6e5ae003e11c839
chessboy71 <bentegrimohamed@gmail.com>
2023-09-13 04:00:27 -0700

    task 103

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -11,7 +11,7 @@
 int main(void)
 {
 	long int fib[100];
-	int i = 0;
+	int i = 2;
 	long int sum = 0;
 
 	fib[0] = 1;
@@ -23,6 +23,7 @@ int main(void)
 		fib[i] = fib[i - 1] + fib[i - 2];
 		if (fib[i] % 2 == 0)
 			sum = sum + fib[i];
+		i++;
 	}
 	printf("%ld \n", sum);
 	return (0);
