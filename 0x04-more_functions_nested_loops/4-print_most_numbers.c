b2e856bdec47842273ff9faa4d565cd5fdf10a72
chessboy71 <bentegrimohamed@gmail.com>
2023-09-14 06:18:57 -0700

    s

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - prints the first 10 numbers without 2 and 4
+ *
+ *
+ * Return: Always 0 (Success)
+*/
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if(i != 2 && i != 4)
+			_putchar(i);
+	}
+	_putchar('\n');
+}
