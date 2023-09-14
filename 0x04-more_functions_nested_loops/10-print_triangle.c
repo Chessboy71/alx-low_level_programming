7069e58469732b1bb991478a6b696eafb524ff50
chessboy71 <bentegrimohamed@gmail.com>
2023-09-14 09:43:23 -0700

    make triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_triangle - prints a triangle
+ * @n : integer
+ *
+ * Return: Always 0 (Success)
+*/
+void print_triangle(int n)
+{
+	int i = 0;
+	int j;
+
+	if (n <= 0)
+		_putchar('\n');
+	while (i < n)
+	{
+		for (j = 0; j < n - ; j++)
+			_putchar(' ');
+		_putchar('#');
+		_putchar('\n');
+		i++;
+	}
+}
