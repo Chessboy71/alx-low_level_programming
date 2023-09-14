5a639176e5d0d8ac864caa1ceca4183bdba4fffa
chessboy71 <bentegrimohamed@gmail.com>
2023-09-14 04:46:50 -0700

    s

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - prints the alphabet ten times
+ *
+ *
+ * Return: Always 0 (Success)
+*/
+void print_numbers(void);
+{	
+	int i; 
+
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
