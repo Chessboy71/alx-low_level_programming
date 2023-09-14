e566a03afd97389b1192db7e19427b882d51cd2b
chessboy71 <bentegrimohamed@gmail.com>
2023-09-14 07:32:19 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * more_numbers - prints the first 10 number 10 times
+ *
+ *
+ * Return: Always 0 (Success)
+*/
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j / 10 > 1)
+				_putchar('0' + j / 10);
+			_putchar('0' + j % 10);
+		}
+		_putchar('\n');
+	}
+}
