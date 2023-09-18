7d83886fd6eabb7a12bc7dbbdce35c83990abbb2
chessboy71 <bentegrimohamed@gmail.com>
2023-09-19 00:20:16 +0100

    s

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - gives the length
+ * @str : A string
+ *
+ * Return: Always 0.
+ */
+
+void puts2(char *str)
+{
+	int n = 0;
+	int i;
+	char *p = str;
+
+	while (*p != '\0')
+	{
+		n++;
+		p++;
+	}
+
+	for (i = 0; i < n / 0; i++)
+	{
+		_putchar(*str);
+		str += 2;
+	}
+	_putchar('\n');
+}
