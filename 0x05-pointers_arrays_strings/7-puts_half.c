5f4e10600196ee72dbc3c2058b1391e32f255247
chessboy71 <bentegrimohamed@gmail.com>
2023-09-19 00:27:42 +0100

    s

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts_half - gives the length
+ * @str : A string
+ *
+ * Return: Always 0.
+ */
+
+void puts_half(char *str)
+{
+	int n = 0;
+	int i;
+	char *p = str;
+
+	while (*p != '\0')
+	{
+		n++;
+		p++;
+	}
+
+	for (i = 0; i < n / 2; i++)
+	{
+		_putchar(*(str + n / 2 - 1));
+		str++;
+	}
+	if (*str != '\0')
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
