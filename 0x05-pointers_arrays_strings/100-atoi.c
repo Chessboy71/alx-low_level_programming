b770e40313fc859c1244a7b83ca27f26ef8a0ade
chessboy71 <bentegrimohamed@gmail.com>
2023-09-19 12:24:44 +0100

    s

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -13,7 +13,7 @@ int _atoi(char *s)
 	unsigned int num = 0;
 	int sign = 1;
 
-	do 
+	do
 	{
 		if (*s == '-')
 		{
@@ -21,7 +21,7 @@ int _atoi(char *s)
 		}
 		else if (*s >= '0' && *s <= '9')
 		{
-			num = num * 10 + (*s - '0'); 
+			num = num * 10 + (*s - '0');
 		}
 		else if (num > 0)
 		{
