5b0c7f058884f85ddb0ecb6dab25a287181065bb
chessboy71 <bentegrimohamed@gmail.com>
2023-09-19 01:29:55 +0100

    s

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - check the code
+ *@dest : pointer to a character
+ *@src : pointer to the source
+ * Return: Always 0.
+ */
+
+char *_strcpy(char *dest, const char *src)
+{
+	char *original_dest = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		src++;
+		dest++;
+	}
+	*dest = '\0';
+	return (original_dest);
+}
+
+
