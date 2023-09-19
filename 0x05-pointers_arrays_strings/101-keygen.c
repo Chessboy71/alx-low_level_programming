7878c86959c1db74efa455b6578dc5c1a67dcc4c
chessboy71 <bentegrimohamed@gmail.com>
2023-09-19 01:57:23 +0100

    s

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,29 +9,23 @@ char generate_random_char()
     int random_choice = rand() % 3;
     if (random_choice == 0)
     {
-        return 'A' + rand() % 26; // Uppercase letter
-    }
+        return 'A' + rand() % 26; 
     else if (random_choice == 1)
     {
-        return 'a' + rand() % 26; // Lowercase letter
-    }
-    else
-    {
-        return '0' + rand() % 10; // Digit
+        return 'a' + rand() % 26;
+        return '0' + rand() % 10;
     }
 }
 
 int main()
 {
-    srand(time(NULL)); // Seed the random number generator with the current time
-
-    char password[PASSWORD_LENGTH + 1]; // +1 for the null terminator
+    srand(time(NULL));
+    char password[PASSWORD_LENGTH + 1];
     for (int i = 0; i < PASSWORD_LENGTH; i++)
     {
         password[i] = generate_random_char();
     }
-    password[PASSWORD_LENGTH] = '\0'; // Null-terminate the password
-
+    password[PASSWORD_LENGTH] = '\0';
     printf("Generated password: %s\n", password);
 
     return 0;
