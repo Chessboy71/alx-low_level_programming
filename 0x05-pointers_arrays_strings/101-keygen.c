4c59794d5ed95a2463684b793b9e03090a93bece
chessboy71 <bentegrimohamed@gmail.com>
2023-09-19 02:02:10 +0100

    s

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,23 +2,6 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define PASSWORD_LENGTH 10
-
-char generate_random_char()
-{
-    int random_choice = rand() % 3;
-
-    if (random_choice == 0)
-    {
-        return 'A' + rand() % 26;
-    } 
-    else if (random_choice == 1)
-    {
-        return 'a' + rand() % 26;
-        return '0' + rand() % 10;
-    }
-}
-
 int main()
 {
     return 0;
