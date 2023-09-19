442b60505732c905d8de00c0f4738480f4692546
chessboy71 <bentegrimohamed@gmail.com>
2023-09-19 01:39:44 +0100

    s

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - check the code
+ *@s : pointer to a character
+ * Return: Always 0.
+ */
+
+int _atoi(char *s)
+{
+	int r = 0;
+
+	while (*s == ' ')
+	{
+		s++;
+	}
+	if (*s == '-')
+	{
+		sign = -1;
+		s++;
+	}
+	else if (*s == '+')
+	{
+		s++;
+	}
+	while (*s >= '0' && *s <= '9')
+	{
+		if (r > INT_MAX / 10 || (r == INT_MAX / 10 && (*s - '0') > INT_MAX % 10))
+		{
+			if (sign == 1)
+			{
+				return (INT_MAX);
+			}
+			else
+			{
+				return (INT_MIN);
+			}
+		}
+		r = r * 10 + (*s - '0');
+		s++;
+		}
+		return (r * sign); }
