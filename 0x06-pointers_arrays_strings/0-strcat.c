a24e67d6cc45cc8c62c7ca6b4088a6d4e72268ba
chessboy71 <bentegrimohamed@gmail.com>
2023-09-20 10:59:26 +0100

    first commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcat - check the code
+ *@dest : pointer to a character
+ *@src : pointer to the source
+ * Return: Always 0.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *original_dest = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+    while( *src != '\0')
+    {
+        *dest = *src;
+        dest++;
+        src++; 
+    }
+    dest++;
+    *dest = '\0';
+
+	return (original_dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+The readme file for the String, arrays and pointers tasks of the **Alx Software engineering curriculum**
+
+Wish us the best the program is still far to end
