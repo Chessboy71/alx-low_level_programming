84b7ffca0a7f9fe8c93a153db128617216a1f8c2
chessboy71 <bentegrimohamed@gmail.com>
2023-09-21 11:45:36 +0100

    third task

---
+++ 0x06-pointers_arrays_strings/1-strncat copy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *_strncpy - check the code
+ *@dest : pointer to a character
+ *@src : pointer to the source
+ *@n : integer to specify the number of bits
+ *Return: Always 0.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *original_dest = dest;
+	int i = 0;
+
+
+	while (*src != '\0' && i < n)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		i++;
+	}
+
+
+
+	return (original_dest);
+}
