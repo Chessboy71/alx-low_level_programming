fa395afb64f3e18a870a5b97836e2f4fd2dceee5
chessboy71 <bentegrimohamed@gmail.com>
2023-09-21 12:08:10 +0100

    s

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,18 +10,14 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int i;
+	
 
 
-	while (*src != '\0' && i < n)
-	{
-		*dest = *src;
-		dest++;
-		src++;
-		i++;
-	}
+   for (i = 0; i < n && src[i] != '\0'; i++)
+        dest[i] = src[i];
+    for ( ; i < n; i++)
+        dest[i] = '\0';
 
-
-
-	return (dest);
+   return (dest);
 }
