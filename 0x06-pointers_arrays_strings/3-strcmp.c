d3131c2baec2446ed741f269a93242190c4d9f8e
chessboy71 <bentegrimohamed@gmail.com>
2023-09-23 00:55:37 +0100

    fourth tak

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings
+ *@s1 : first string
+ *@s2 : second string
+ *Return: Always 0.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int eq;
+
+	while (*s1 != '\0')
+	{
+		if (*s1 != *s2)
+		{
+			eq = ((int)*s1 - 48) - ((int)*s2 - 48);
+			break;
+		}
+		s1++;
+		s2++;
+	}
+	return (eq);
+}
