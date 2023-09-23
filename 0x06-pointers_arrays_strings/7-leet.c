0fc7c230d16bb2e3d0371da88e338753470e9f37
chessboy71 <bentegrimohamed@gmail.com>
2023-09-23 01:24:54 +0100

    task 5

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *reverse_array - check the code
+ *@a : pointer to array
+ *@n : number of cases
+ *Return: Always 0.
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, j, t;
+
+	for (i = 0, j = n - 1; i < j; i++, j--)
+	{
+		t = a[i];
+		a[i] = a[j];
+		a[j] = t;
+	}
+}
