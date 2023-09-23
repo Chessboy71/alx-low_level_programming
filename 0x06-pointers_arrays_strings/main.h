f6f38aabf8584edd6ea53898b11b3d8abc8de725
chessboy71 <bentegrimohamed@gmail.com>
2023-09-23 03:03:18 +0100

    s

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,3 +9,6 @@ int _putchar(char c);
 int isDelimiter(char c);
 int isLower(char c);
 void rev_string(char *n);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
