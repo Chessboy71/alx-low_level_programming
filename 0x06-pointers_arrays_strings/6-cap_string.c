953c664a51943e985369d7cd39a0416807a87f6c
chessboy71 <bentegrimohamed@gmail.com>
2023-09-23 01:58:22 +0100

    second task

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -20,9 +20,9 @@ int isLower(char c)
 int isDelimiter(char c)
 {
 	int i;
-	char delimiter[] = " ;\t\n,.!?\"(){}";
+	char delimiter[] = " \t\n,.!?\"(){}";
 
-	for (i = 0; i < 13; i++)
+	for (i = 0; i < 12; i++)
 	{
 		if (c == delimiter[i])
 			return (1);
