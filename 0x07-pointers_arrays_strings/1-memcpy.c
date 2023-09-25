3d1fa41aaf638238a63c073582c50bb2b41d5942
chessboy71 <bentegrimohamed@gmail.com>
2023-09-25 03:39:26 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memcpy - prints buffer in hexa
+ * @dest: the pointer to the string of destination
+ * @src : the pointer to the source string
+ * @n : how many bytes to print
+ *
+ * Return: pointer to destination
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *str = src;
+
+	while (n != 0)
+	{
+		*str = *src;
+		n--;
+		str++;
+	}
+	return (dest);
+}
