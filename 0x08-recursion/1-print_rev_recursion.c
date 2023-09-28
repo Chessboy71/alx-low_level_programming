c419cd3821e34e695676f27aadeed287a6bc6f8a
chessboy71 <bentegrimohamed@gmail.com>
2023-09-28 22:04:10 +0100

    print rev recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,21 +1,6 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - prints a string
- * @s: the string to get the length
- *
- * Return: nothing
- */
-int _strlen_recursion(char *s)
-{
-	if (*s)
-	{
-		return (_strlen_recursion(s + 1) + 1);
-	}
-	else
-		return (0);
-}
-/**
  * _puts_recursion - prints a string
  * @s: the string to print
  *
@@ -25,8 +10,8 @@ void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
-		_putchar(*s);
 		_puts_recursion(s + 1);
+		_putchar(*s);
 	}
 	else
 	{
