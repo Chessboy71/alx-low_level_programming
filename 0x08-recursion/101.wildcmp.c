8e9a9cea29a4cfcf9b4094c5ebb4f7ce7a547563
chessboy71 <bentegrimohamed@gmail.com>
2023-09-29 01:30:43 +0100

    last task

---
+++ 0x08-recursion/101.wildcmp.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * wildcmp - checks the characters recursively for palindrome
+ * @s1: string to check
+ * @s2: iterator
+ *
+ * Return: 1 if relates, 0 if not
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
