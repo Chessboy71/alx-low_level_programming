2aaf07fd7d434d6007eed324a6cadf6ceac92b9b
chessboy71 <bentegrimohamed@gmail.com>
2023-10-04 23:40:18 +0100

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strlen - getlength of a string,
+ *
+ *@str : the character to initialize
+ *
+ *Return: void
+ */
+
+int _strlen(char *str)
+{
+	int n = 0;
+	char *p = str;
+
+	while (*p != '\0')
+	{
+		n++;
+		p++;
+	}
+	return (n);
+}
+/**
+ * _strdup - creates an array of chars,
+ * and initializes it with a specific char
+ *@str : size of the array
+ *
+ *Return: void
+ */
+char *_strdup(char *str)
+{
+	char *ret;
+	int i = _strlen(str);
+	char *s = (char *)malloc(_strlen(str) * sizeof(char));
+
+	if (s == 0)
+		return (NULL);
+	ret = s;
+	while (i > 0)
+	{
+		*s = *str;
+		s++;
+		str++;
+		i--;
+	}
+	return (ret);
+}
