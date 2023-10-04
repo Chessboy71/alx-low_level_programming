d24ceab2aab8b0fe015b320f1a867283a0486c8f
chessboy71 <bentegrimohamed@gmail.com>
2023-10-04 23:18:07 +0100

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,13 +12,13 @@
  */
 char *create_array(unsigned int size, char c)
 {
-    char *arr = (char *)malloc(size * sizeof(char));
-    unsigned int i;
+	char *arr = (char *)malloc(size * sizeof(char));
+	unsigned int i;
 
-    if (size == 0 || arr == NULL)
-        return (NULL);
-    for (i = 0; i < size; i++)
-        arr[i] = c;
-    arr[size] = '\0';
-    return (arr);
+	if (size == 0 || arr == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		arr[i] = c;
+	arr[size] = '\0';
+	return (arr);
 }
