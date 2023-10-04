060859291f149fe7abb8b9c7f60b48f77de0af1c
chessboy71 <bentegrimohamed@gmail.com>
2023-10-04 22:56:28 +0100

    starting today tasks

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,12 +12,10 @@
 char *create_array(unsigned int size, char c)
 {
     char *arr = (char *)malloc(size * sizeof(char));
-    int i;
+    unsigned int i;
 
     if (size == 0)
         return NULL;
-    // if (*arr == NULL)
-    //     return NULL;
     for (i = 0; i < size; i++)
         arr[i] = c;
     arr[size] = '\0';
