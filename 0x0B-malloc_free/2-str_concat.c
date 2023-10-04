0815e697b6333d66c3a52c845454da482524ab19
chessboy71 <bentegrimohamed@gmail.com>
2023-10-05 00:56:48 +0100

    str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -37,7 +37,7 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL && s2 == NULL)
 	{
 		newstr = (char *)malloc(1);
-		*newstr = "";
+		newstr = "";
 		return (newstr);
 	}
 	else if (s1 == NULL)
