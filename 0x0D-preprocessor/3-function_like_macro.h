5c7aa05cd6d1d2ca802986a3d882aa0840a162e0
chessboy71 <bentegrimohamed@gmail.com>
2023-10-10 13:31:53 +0100

    function like macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
 #ifndef FUNCTION_LIKE
 #define FUNCTION_LIKE
 
-#define ABS(x) ((x) < 0 ? -(x) : (x))
-#endif \* FUNCTION LIKE *\
+#define ABS(x) ((x) < 0 ? (-(x)) : (x))
+#endif /*FUNCTION_LIKE*/
 
