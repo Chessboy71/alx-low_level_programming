445a719266e9b0fdd2cffa1d8d53e373109364ae
chessboy71 <bentegrimohamed@gmail.com>
2023-10-12 00:16:29 +0100

    int_index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,13 +14,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (size <= 0)
+	if (!array || !size || !cmp)
+		return (-1);
+	else if (size <= 0)
 		return (-1);
 
 	while (i < size)
 	{
-		if (cmp(*(array + i)) != 0)
-			return (*(array + 1));
+		if (cmp(array[i]))
+			return (i);
 		i++;
 	}
 	return (-1);
