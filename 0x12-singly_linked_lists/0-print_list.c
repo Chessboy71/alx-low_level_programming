e8e530f40f44728325afed4126c44f8e152378f7
chessboy71 <bentegrimohamed@gmail.com>
2023-10-20 22:31:48 +0100

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,10 +13,14 @@ size_t print_list(const list_t *h)
     while (h != NULL)
     {
         if (!h->str)
-            printf("[0] (nil)");
-        printf("[%u]", h->len);
-        printf(" %s\n", h->str);
+            printf("[0] (nil)\n");
+        else
+        {
+            printf("[%u]", h->len);
+            printf(" %s\n", h->str);
+        }
         h = h->next;
+
         count += 1;
     }
     return (count);
