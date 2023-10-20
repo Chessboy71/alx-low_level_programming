626e4150f245d1e9c13f9a2ba9ccdb86838ab800
chessboy71 <bentegrimohamed@gmail.com>
2023-10-20 22:30:08 +0100

    print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_list - prints the strings of a linked list
+ *
+ * @h : the linked lists head
+ * Return: the number of the items
+ */
+
+size_t print_list(const list_t *h)
+{
+    size_t count = 0;
+    while (h != NULL)
+    {
+        if (!h->str)
+            printf("[0] (nil)");
+        printf("[%u]", h->len);
+        printf(" %s\n", h->str);
+        h = h->next;
+        count += 1;
+    }
+    return (count);
+}
