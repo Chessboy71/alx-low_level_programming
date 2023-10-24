1fc561b5686ef8eb54672ae6adcf026e57768ff6
chessboy71 <bentegrimohamed@gmail.com>
2023-10-24 20:36:12 +0100

    delete nodeint

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *tmp, *t;
 	unsigned int i = 0;
 
-	if (!head || *head == NULL)
+	if (!head || *head == NULL || index == NULL)
 		return (-1);
 
 	tmp = *head;
