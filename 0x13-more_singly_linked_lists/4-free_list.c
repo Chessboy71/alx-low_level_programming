ecd90c6b64b5ba58d5a3127340703ef65dd343f6
chessboy71 <bentegrimohamed@gmail.com>
2023-10-24 11:34:12 +0100

    task 0

--- 0x13-more_singly_linked_lists/4-free_list.c
+++ 0x13-more_singly_linked_lists/4-free_list.c
@@ -1,18 +1,18 @@
-#include "lists.h"
+// #include "lists.h"
 
-/**
- * free_list - frees a linked list
- * @head: list_t list to be freed
- */
-void free_list(list_t *head)
-{
-	list_t *t;
+// /**
+//  * free_list - frees a linked list
+//  * @head: list_t list to be freed
+//  */
+// void free_list(list_t *head)
+// {
+// 	list_t *t;
 
-	while (head)
-	{
-		t = head->next;
-		free(head->str);
-		free(head);
-		head = t;
-	}
-}
+// 	while (head)
+// 	{
+// 		t = head->next;
+// 		free(head->str);
+// 		free(head);
+// 		head = t;
+// 	}
+// }
