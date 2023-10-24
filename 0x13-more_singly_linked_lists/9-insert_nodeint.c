adb975393ff5e81161aa06d8755b72281c5015f6
chessboy71 <bentegrimohamed@gmail.com>
2023-10-24 22:48:50 +0100

    add node

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - insert node at nth place
+ * @head: the head of the list
+ * @idx: index of the node
+ * @n: the number
+ * Return: the sum of the list
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i;
+	listint_t *new;
+	listint_t *tmp = *head;
+
+	new = malloc(sizeof(listint_t));
+	if (!new || !head)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+
+	for (i = 0; tmp && i < idx; i++)
+	{
+		if (i == idx - 1)
+		{
+			new->next = tmp->next;
+			tmp->next = new;
+			return (new);
+		}
+		else
+			tmp = tmp->next;
+	}
+
+	return (NULL);
+}
