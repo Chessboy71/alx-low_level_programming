0696085f2aedc126ef849573aa7d154a86c6dbf4
chessboy71 <bentegrimohamed@gmail.com>
2023-10-24 11:45:42 +0100

    fixing betty

--- 0x13-more_singly_linked_lists/1-list_len.c
+++ 0x13-more_singly_linked_lists/1-list_len.c
@@ -1,17 +1,17 @@
 #include "lists.h"
 
 /**
- * list_len - prints the strings of a linked list
+ * listint_len - prints the strings of a linked list
  *
  * @h : the linked lists head
  * Return: the number of the items
  */
 
-size_t list_len(const list_t *h)
+size_t listint_len(const listint_t *h)
 {
 	size_t count = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		h = h->next;
 		count += 1;
