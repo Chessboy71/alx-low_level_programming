dfb6424b36bc6772875e09ef3795c8a13f1a7d90
chessboy71 <bentegrimohamed@gmail.com>
2023-10-24 19:08:16 +0100

    adding node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * add_node - adds a new node at the beginning of a linked list
+ * add_nodeint - adds a new node at the beginning of a linked list
  * @head: points ot the head of the list
  * @n: new integer
  *
