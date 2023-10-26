7b2d6a756fea78eabff65f2d4df3bb518c484d33
chessboy71 <bentegrimohamed@gmail.com>
2023-10-26 20:35:03 +0100

    binary to int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,12 +11,12 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num = 0;
 
-	if (!b)
+	if (b == NULL)
 		return (0);
 
 	while (*b)
 	{
-		if (*b != 0 || *b != 1)
+		if (*b != '0' && *b != '1')
 			return (0);
 		num = num * 2 + (*b++ - '0');
 	}
