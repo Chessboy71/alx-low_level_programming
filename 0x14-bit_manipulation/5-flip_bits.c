6afc7b468f9c996329379e553795d71d7cd171dd
chessboy71 <bentegrimohamed@gmail.com>
2023-10-26 23:07:04 +0100

    xooor

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * flip_bits - calculates the number of bits that flip
+ *
+ * @n : teh first number
+ * @index: the second number
+ *
+ * Return: the number of bits that are different
+ *
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int xr = n ^ m;
+	unsigned long int i = 0;
+
+	while (xr)
+	{
+		if (xr & 1ul)
+			i++;
+		xr = xr >> 1;
+	}
+	return (i);
+}
