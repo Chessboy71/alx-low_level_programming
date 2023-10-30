4a9eedb10d75a0801f27c7d55bdf6eb3d07e4522
chessboy71 <bentegrimohamed@gmail.com>
2023-10-30 21:22:54 +0100

    rewriting

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,10 +18,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (!filename || !letters)
 		return (0);
 	f = open(filename, O_RDONLY);
-	if(f == -1)
+	if (f == -1)
 		return (0);
 	res = read(f, &buff[0], letters);
 	res = write(STDOUT_FILENO, &buff[0], letters);
 	close(f);
-	return(res);
+	return (res);
 }
