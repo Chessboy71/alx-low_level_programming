64fa86ca353f3532ff5f8e38a4f503affbbda0b3
chessboy71 <bentegrimohamed@gmail.com>
2023-10-30 23:31:01 +0100

    s

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,24 +1,5 @@
 #include "main.h"
 /**
- * _strlen - gives the length
- * @str : A string
- *
- * Return: Always 0.
- */
-int _strlen(char *str)
-{
-	int n = 0;
-	char *p = str;
-
-	while (*p != '\0')
-	{
-		n++;
-		p++;
-	}
-	return (n);
-}
-
-/**
  * create_file - creates file
  *
  * @filename : the filename to start
@@ -30,15 +11,15 @@ int _strlen(char *str)
 int create_file(const char *filename, char *text_content)
 {
 	int f;
-	ssize_t res = 0, len = _strlen(text_content);
+	ssize_t res = 0, len;
 
 	if (!filename)
 		return (-1);
 	f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (f == -1)
 		return (-1);
-	if (!text_content)
-		text_content = "";
+	for (len = 0; text_content[len]; len++)
+		;
 	if (len)
 		res = write(f, text_content, len);
 	close(f);
