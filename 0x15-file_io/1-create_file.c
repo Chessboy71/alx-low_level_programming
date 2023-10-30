4d8da51ca66bb8a961bdff8aaafa3986e5f22976
chessboy71 <bentegrimohamed@gmail.com>
2023-10-30 23:27:44 +0100

    s

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -37,6 +37,8 @@ int create_file(const char *filename, char *text_content)
 	f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (f == -1)
 		return (-1);
+	if (!text_content)
+		text_content = "";
 	if (len)
 		res = write(f, text_content, len);
 	close(f);
