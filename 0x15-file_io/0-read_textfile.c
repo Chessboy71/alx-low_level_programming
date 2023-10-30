9213679e62167b45fd1018fa5264f7585d92d2f3
chessboy71 <bentegrimohamed@gmail.com>
2023-10-30 17:53:10 +0100

    starting out

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * read_textfile - converts a binary to uint
+ *
+ * @b : the string
+ * Return: the converted number or 0 if failed
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int f;
+	ssize_t res;
+	char buff[READ_BUF_SIZE * 8];
+
+	if (!filename || !letters)
+		return (0);
+	f = open(filename, O_RDONLY);
+	if(fd == -1)
+		return (0);
+	res = read(f, buff[0], letters);
+	res = write(STDOUT_FILENO, buff[0], letters);
+	close(f);
+	return(res);
+	
+
+}
